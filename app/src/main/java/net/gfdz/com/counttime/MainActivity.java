c5df947f0c7d9a44653cde36e9d96e2f8e7c1a19
xiaobu <1172504039@qq.com>
2015-11-25 09:26:58 +0800

    计时器

--- app/src/main/java/net/gfdz/com/counttime/MainActivity.java
+++ app/src/main/java/net/gfdz/com/counttime/MainActivity.java
@@ -1,16 +1,33 @@
 package net.gfdz.com.counttime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText inputEt;
     private Button btnGetTime,btnStart,btnStop;
     private TextView time;
+    private int  i=0;
+    private Timer timer=null;
+    private TimerTask tt;
+    private Handler mHandler=new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            time.setText(msg.arg1+"");
+            startTime();
+        }
+    };
 
 
     @Override
@@ -33,12 +50,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
       switch (v.getId()){
           case R.id.btnGetTime:
-              time.setText(inputEt.getText().toString());
+              time.setText(inputEt.getText());
+              i=Integer.parseInt(inputEt.getText().toString());
               break;
           case R.id.btnStart:
+              startTime();
               break;
           case R.id.btnStop:
+              StopTime();
               break;
       }
+    }
+    public void startTime(){
+        timer=new Timer();
+        tt=new TimerTask() {
+            @Override
+            public void run() {
+                i--;
+                Message msg=mHandler.obtainMessage();
+                msg.arg1=i;
+                mHandler.sendMessage(msg);
+
+            }
+        };
+        timer.schedule(tt,1000);
+    }
+    public void StopTime(){
+      timer.cancel();
     }
 }
